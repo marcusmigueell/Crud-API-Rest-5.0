c3751eaa1e9910fd903565334f3022fe8e40457e
MarcusMiguel <marcus.miguel@a2wplataforma.com.br>
2022-05-31 20:46:28 -0300

    API V1 Update

---
+++ FilmesAPI/Profiles/FIlmeProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using FilmesAPI.Data.Dtos;
+using FilmesAPI.Models;
+
+namespace FilmesAPI.Profiles
+{
+    public class FIlmeProfile : Profile {
+        
+        public FIlmeProfile() {
+            CreateMap<CreateFilmeDto, Filme>();
+            CreateMap<UpdateFilmeDto, Filme>();
+            CreateMap<Filme, ReadFilmeDto>();
+        }
+        
+    }
+}
